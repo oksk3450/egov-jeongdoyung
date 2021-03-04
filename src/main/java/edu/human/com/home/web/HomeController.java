8e9398667121cf544f8608e00f1bc12b8636afda
82107 <82107@DESKTOP-Q5DLQ0C>
2021-03-04 17:41:29 +0900

    20210304 결과

--- src/main/java/edu/human/com/home/web/HomeController.java
+++ src/main/java/edu/human/com/home/web/HomeController.java
@@ -1,15 +1,181 @@
 package edu.human.com.home.web;
 
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import edu.human.com.util.CommonUtil;
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.cop.bbs.service.BoardMasterVO;
+import egovframework.let.cop.bbs.service.BoardVO;
+import egovframework.let.cop.bbs.service.EgovBBSAttributeManageService;
+import egovframework.let.cop.bbs.service.EgovBBSManageService;
+import egovframework.rte.fdl.property.EgovPropertyService;
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+
 @Controller
 public class HomeController {
 	
+	@Autowired //자바8버전 나오기전 많이사용 자바8이후 @Inject로 사용 아주예전 @Resource
+	private EgovBBSAttributeManageService bbsAttrbService;
+	@Autowired
+	private EgovPropertyService propertyService;
+	@Autowired
+	private EgovBBSManageService bbsMngService;
+	@Autowired
+	private EgovMessageSource egovMessageSource;
+	
+	@Inject
+	private CommonUtil commUtil;
+	
+	@RequestMapping("/tiles/board/insert_board.do")
+	public String insert_board_form(@ModelAttribute("searchVO") BoardVO boardVO, ModelMap model) throws Exception {
+		// 사용자권한 처리
+		if(!EgovUserDetailsHelper.isAuthenticated()) {
+			model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
+	    	return "cmm/uat/uia/EgovLoginUsr";
+		}
+
+	    LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+
+		BoardMasterVO bdMstr = new BoardMasterVO();
+
+		if (isAuthenticated) {
+
+		    BoardMasterVO vo = new BoardMasterVO();
+		    vo.setBbsId(boardVO.getBbsId());
+		    vo.setUniqId(user.getUniqId());
+
+		    bdMstr = bbsAttrbService.selectBBSMasterInf(vo);
+		    model.addAttribute("bdMstr", bdMstr);
+		}
+
+		//----------------------------
+		// 기본 BBS template 지정
+		//----------------------------
+		if (bdMstr.getTmplatCours() == null || bdMstr.getTmplatCours().equals("")) {
+		    bdMstr.setTmplatCours("/css/egovframework/cop/bbs/egovBaseTemplate.css");
+		}
+
+		model.addAttribute("brdMstrVO", bdMstr);
+		////-----------------------------
+
+		return "board/insert_board.tiles";
+	}
+	
+	@RequestMapping("/tiles/board/view_board.do")
+	public String view_board(@ModelAttribute("searchVO") BoardVO boardVO, ModelMap model) throws Exception {
+		LoginVO user = new LoginVO();
+	    if(EgovUserDetailsHelper.isAuthenticated()){
+	    	user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		}
+
+		// 조회수 증가 여부 지정
+		boardVO.setPlusCount(true);
+
+		//---------------------------------
+		// 2009.06.29 : 2단계 기능 추가
+		//---------------------------------
+		if (!boardVO.getSubPageIndex().equals("")) {
+		    boardVO.setPlusCount(false);
+		}
+		////-------------------------------
+
+		boardVO.setLastUpdusrId(user.getUniqId());
+		BoardVO vo = bbsMngService.selectBoardArticle(boardVO);
+		//시큐어코딩 시작(게시물제목/내용에서 자바스크립트 코드의 꺽쇠태그를 특수문자로 바꿔서 실행하지 못하는 코드로 변경)
+		//egov 저장할때, 시큐어코딩으로 저장하는 방식을 사용, 문제있음. 우리방식으로 적용
+		String subject = commUtil.unscript(vo.getNttSj());//게시물제목
+		String content = commUtil.unscript(vo.getNttCn());//개시물내용
+		vo.setNttSj(subject);
+		vo.setNttCn(content);
+		model.addAttribute("result", vo);
+		
+		model.addAttribute("sessionUniqId", user.getUniqId());
+
+		//----------------------------
+		// template 처리 (기본 BBS template 지정  포함)
+		//----------------------------
+		BoardMasterVO master = new BoardMasterVO();
+
+		master.setBbsId(boardVO.getBbsId());
+		master.setUniqId(user.getUniqId());
+
+		BoardMasterVO masterVo = bbsAttrbService.selectBBSMasterInf(master);
+
+		if (masterVo.getTmplatCours() == null || masterVo.getTmplatCours().equals("")) {
+		    masterVo.setTmplatCours("/css/egovframework/cop/bbs/egovBaseTemplate.css");
+		}
+
+		model.addAttribute("brdMstrVO", masterVo);
+		
+		return "board/view_board.tiles";//.tiles로 리턴받으면, 루트가 tiles폴더가 루트가되고, view_board.jsp내용이 content영역에 나오게 됨.
+	}
+
 	@RequestMapping("/tiles/board/list_board.do")
-	public String list_board() throws Exception {
+	public String list_board(@ModelAttribute("searchVO") BoardVO boardVO, ModelMap model) throws Exception {
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+
+		boardVO.setBbsId(boardVO.getBbsId());
+		boardVO.setBbsNm(boardVO.getBbsNm());
+
+		BoardMasterVO vo = new BoardMasterVO();
+		System.out.println("디버그: 게시판아이디는 "+boardVO.getBbsId());
+		vo.setBbsId(boardVO.getBbsId());
+		vo.setUniqId(user.getUniqId());
+
+		BoardMasterVO master = bbsAttrbService.selectBBSMasterInf(vo);
+
+		//-------------------------------
+		// 방명록이면 방명록 URL로 forward
+		//-------------------------------
+		if (master.getBbsTyCode().equals("BBST04")) {
+		    return "forward:/cop/bbs/selectGuestList.do";
+		}
+		////-----------------------------
+
+		boardVO.setPageUnit(propertyService.getInt("pageUnit"));
+		boardVO.setPageSize(propertyService.getInt("pageSize"));
+
+		PaginationInfo paginationInfo = new PaginationInfo();
+
+		paginationInfo.setCurrentPageNo(boardVO.getPageIndex());
+		paginationInfo.setRecordCountPerPage(boardVO.getPageUnit());
+		paginationInfo.setPageSize(boardVO.getPageSize());
+
+		boardVO.setFirstIndex(paginationInfo.getFirstRecordIndex());
+		boardVO.setLastIndex(paginationInfo.getLastRecordIndex());
+		boardVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
+
+		Map<String, Object> map = bbsMngService.selectBoardArticles(boardVO, vo.getBbsAttrbCode());
+		int totCnt = Integer.parseInt((String)map.get("resultCnt"));
+
+		paginationInfo.setTotalRecordCount(totCnt);
+
+		//-------------------------------
+		// 기본 BBS template 지정
+		//-------------------------------
+		if (master.getTmplatCours() == null || master.getTmplatCours().equals("")) {
+		    master.setTmplatCours("/css/egovframework/cop/bbs/egovBaseTemplate.css");
+		}
+		////-----------------------------
+
+		model.addAttribute("resultList", map.get("resultList"));
+		model.addAttribute("resultCnt", map.get("resultCnt"));
+		model.addAttribute("boardVO", boardVO);
+		model.addAttribute("brdMstrVO", master);
+		model.addAttribute("paginationInfo", paginationInfo);
 		
 		return "board/list_board.tiles";
 	}
